3ceae846647bdf725881ef819e79c86285d24706
FilipeGomesFreire <162646231+FilipeGomesFreire@users.noreply.github.com>
2025-03-14 14:40:55 +0000

    atividade

---
+++ springboot-server/springboot-server/src/test/java/com/seuprojeto/SpringbootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
